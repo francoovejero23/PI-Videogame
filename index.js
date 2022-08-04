658401054456fdcbaa1700b163fba579bb84314a
francoovejero23 <83842963+francoovejero23@users.noreply.github.com>
2022-08-04 01:59:14 -0300

    cambiando port

--- index.js
+++ index.js
@@ -21,15 +21,16 @@ const { default: axios } = require('axios');
 const server = require('./src/app.js');
 const { conn, Genre } = require('./src/db.js');
 const { API_KEY } = process.env;
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
-  server.listen(3001, async () => {
+  server.listen(PORT, async () => {
     const infoApi = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
     const content = infoApi.data.results;
     content.forEach(async el => {
       await Genre.create({name: el.name})
     })
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`Listen on port ${PORT}`); // eslint-disable-line no-console
   });
 });
